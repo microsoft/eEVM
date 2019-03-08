24be26b00ed91a4bc1fa0187c639e25919e65124
Eddy Ashton <ashton.eddy@gmail.com>
2019-03-08 14:49:00 +0000

    Replace RLP implementation (#19)
    * Clear error message when TEST_DIR is not set
    
    * Use uint256 call_values
    
    * In progress improvements to RLP
    
    Working tuple nesting
    
    * Tuple of tuples is still a problem
    
    * Correct and tested RLP
    
    * Add comments
    
    * Replace old rlp-implementation with new
    
    * Rename rlp::to_bytes to to_byte_string
    
    * Add to_byte_string for boost bigints
    
    * Test large uint256_t encoding
    
    * Remove debug inclusions
    
    * Add missed include for std::apply
    
    * Print verbose make instructions in CI
    
    * Try a windows-hosted agent
    
    * Reinstate Ubuntu CI - give up on it passing

---
+++ include/rlp.h
@@ -0,0 +1,193 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+#pragma once
+
+#include <cstdint>
+#include <string>
+#include <tuple>
+#include <utility>
+#include <vector>
+
+namespace evm
+{
+  namespace rlp
+  {
+    using ByteString = std::vector<uint8_t>;
+
+    // Helper to detect tuples
+    namespace
+    {
+      template <typename... Ts>
+      struct is_tuple : std::false_type
+      {};
+
+      template <typename... Ts>
+      struct is_tuple<std::tuple<Ts...>> : std::true_type
+      {};
+    }
+
+    // Forward declaration to allow recursive calls.
+    template <typename... Ts>
+    ByteString encode(Ts&&... ts);
+
+    inline ByteString to_byte_string(const ByteString& bs)
+    {
+      return bs;
+    }
+
+    inline ByteString to_byte_string(const std::string& s)
+    {
+      return ByteString(s.begin(), s.end());
+    }
+
+    inline ByteString to_byte_string(uint64_t n)
+    {
+      // "positive RLP integers must be represented in big endian binary form
+      // with no leading zeroes"
+      bool started = false;
+
+      ByteString bs;
+      for (size_t byte_index = 0; byte_index < sizeof(n); ++byte_index)
+      {
+        const uint64_t offset = (7 - byte_index) * 8;
+        const uint64_t mask = 0xff << offset;
+
+        const uint8_t current_byte = (mask & n) >> offset;
+
+        if (started || current_byte != 0)
+        {
+          bs.push_back(current_byte);
+          started = true;
+        }
+      }
+
+      // NB: If n is 0, this returns an empty list - this is correct.
+      return bs;
+    }
+
+    inline ByteString to_byte_string(const uint256_t& n)
+    {
+      ByteString bs;
+      boost::multiprecision::export_bits(n, std::back_inserter(bs), 8, true);
+      return bs;
+    }
+
+    // RLP-encode a single, non-tuple argument. Convert it to a ByteString,
+    // prefix with the encoded length.
+    template <typename T>
+    ByteString encode_single(const T& t)
+    {
+      auto bytes = to_byte_string(t);
+      const auto length = bytes.size();
+
+      // "For a single byte whose value is in the [0x00, 0x7f] range, that
+      // byte is its own RLP encoding."
+      if (length == 1 && bytes[0] <= 0x7f)
+      {
+        return bytes;
+      }
+
+      // "Otherwise, if a string is 0-55 bytes long, the RLP encoding
+      // consists of a single byte with value 0x80 plus the length of the
+      // string followed by the string. The range of the first byte is thus
+      // [0x80, 0xb7]."
+      if (length <= 55)
+      {
+        bytes.insert(bytes.begin(), 0x80 + length);
+        return bytes;
+      }
+
+      // "If a string is more than 55 bytes long, the RLP encoding consists of
+      // a single byte with value 0xb7 plus the length in bytes of the length
+      // of the string in binary form, followed by the length of the string,
+      // followed by the string. For example, a length-1024 string would be
+      // encoded as \xb9\x04\x00 followed by the string. The range of the
+      // first byte is thus [0xb8, 0xbf]."
+      auto length_as_bytes = to_byte_string(length);
+      const uint8_t length_of_length = length_as_bytes.size();
+
+      length_as_bytes.insert(length_as_bytes.begin(), 0xb7 + length_of_length);
+      bytes.insert(
+        bytes.begin(), length_as_bytes.begin(), length_as_bytes.end());
+      return bytes;
+    }
+
+    // RLP-encode a list of arguments. Encode each separately, and return a
+    // tuple of the results.
+    template <
+      typename... Ts,
+      typename = std::enable_if_t<!is_tuple<std::decay_t<Ts>...>::value>>
+    auto encode_multiple(Ts&&... ts)
+    {
+      return std::make_tuple(encode(ts)...);
+    }
+
+    // RLP-encode a tuple argument. Treat this as a list of multiple of
+    // arguments - encode each separately, and return a tuple of the results.
+    // This allows nested heterogeneous lists to be represented as tuples.
+    template <typename... Ts>
+    auto encode_multiple(const std::tuple<Ts...>& tup)
+    {
+      return std::apply(
+        [](auto&&... entry) { return std::make_tuple(encode(entry)...); }, tup);
+    }
+
+    // Main encode function. Either forwards to encode_single, or calls
+    // encode_multiple then prepends list-length encoding to concatenated
+    // results.
+    template <typename... Ts>
+    ByteString encode(Ts&&... ts)
+    {
+      if constexpr (sizeof...(Ts) == 1 && !is_tuple<std::decay_t<Ts>...>::value)
+      {
+        return encode_single(std::forward<Ts>(ts)...);
+      }
+
+      const auto nested_terms = encode_multiple(std::forward<Ts>(ts)...);
+
+      // Get total length by summing size of each term in tuple
+      const auto total_length = std::apply(
+        [](auto&&... term) { return (term.size() + ... + 0); }, nested_terms);
+
+      ByteString flattened;
+
+      // Get concatenation by inserting each term's encoding
+      std::apply(
+        [&flattened](auto&&... term) {
+          ((flattened.insert(flattened.end(), term.begin(), term.end()), ...));
+        },
+        nested_terms);
+
+      // "If the total payload of a list (i.e. the combined length of all its
+      // items being RLP encoded) is 0-55 bytes long, the RLP encoding
+      // consists of a single byte with value 0xc0 plus the length of the list
+      // followed by the concatenation of the RLP encodings of the items. The
+      // range of the first byte is thus [0xc0, 0xf7]."
+      // NB: This _should_ say '0xc0 plus the length of the concatenation of
+      // RLP encoding of the items'
+      if (total_length <= 55)
+      {
+        flattened.insert(flattened.begin(), 0xc0 + total_length);
+        return flattened;
+      }
+
+      // "If the total payload of a list is more than 55 bytes long, the RLP
+      // encoding consists of a single byte with value 0xf7 plus the length in
+      // bytes of the length of the payload in binary form, followed by the
+      // length of the payload, followed by the concatenation of the RLP
+      // encodings of the items. The range of the first byte is thus [0xf8,
+      // 0xff]."
+      auto total_length_as_bytes = to_byte_string(total_length);
+      const uint8_t length_of_total_length = total_length_as_bytes.size();
+
+      total_length_as_bytes.insert(
+        total_length_as_bytes.begin(), 0xf7 + length_of_total_length);
+      flattened.insert(
+        flattened.begin(),
+        total_length_as_bytes.begin(),
+        total_length_as_bytes.end());
+      return flattened;
+    }
+  }
+} // namespace evm
--- tests/main.cpp
+++ tests/main.cpp
@@ -1,11 +1,11 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
-#include "../evm/rlp.h"
 #include "../evm/simpleglobalstate.h"
 #include "../include/disassembler.h"
 #include "../include/opcode.h"
 #include "../include/processor.h"
+#include "../include/rlp.h"
 #include "../include/util.h"
 
 #define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
@@ -133,68 +133,81 @@ TEST_CASE("byteExport" * doctest::test_suite("primitive"))
 
 TEST_CASE("rlp" * doctest::test_suite("rlp"))
 {
-  SUBCASE("constructors")
-  {
-    rlp::ByteString bs{0xde, 0xad, 0xbe, 0xef};
-    rlp::Item a(bs);
-    CHECK(a.type == rlp::Item::Single);
-
-    rlp::Item::TList l{rlp::to_byte_string("cat"), rlp::to_byte_string("dog")};
-    rlp::Item b(l);
-    CHECK(b.type == rlp::Item::List);
-
-    CHECK(a != b);
-
-    rlp::Item c = a;
-    CHECK(a == c);
-
-    rlp::Item d = b;
-    CHECK(b == d);
-
-    CHECK(c != d);
-  }
-
   SUBCASE("encode")
   {
-    CHECK(rlp::encode("dog") == rlp::ByteString{0x83, 'd', 'o', 'g'});
+    CHECK(rlp::encode(0x0) == rlp::ByteString{0x80});
+    CHECK(rlp::encode(0x1) == rlp::ByteString{0x1});
+    CHECK(rlp::encode(0x7f) == rlp::ByteString{0x7f});
+    CHECK(rlp::encode(0x80) == rlp::ByteString{0x81, 0x80});
 
-    rlp::Item::TList catdog{rlp::to_byte_string("cat"),
-                            rlp::to_byte_string("dog")};
-    CHECK(
-      rlp::encode(catdog) ==
-      rlp::ByteString{0xc8, 0x83, 'c', 'a', 't', 0x83, 'd', 'o', 'g'});
-
+    CHECK(rlp::encode() == rlp::ByteString{0xc0});
     CHECK(rlp::encode("") == rlp::ByteString{0x80});
 
-    rlp::Item::TList empty{};
-    CHECK(rlp::encode(empty) == rlp::ByteString{0xc0});
+    CHECK(rlp::encode(0x0, 0x0) == rlp::ByteString{0xc2, 0x80, 0x80});
+    CHECK(rlp::encode(0x1, 0x2, 0x3) == rlp::ByteString{0xc3, 0x1, 0x2, 0x3});
 
-    rlp::ByteString int0{0x00};
-    CHECK(rlp::encode(int0) == int0);
-
-    rlp::ByteString int15{0x0f};
-    CHECK(rlp::encode(int15) == int15);
-
-    rlp::ByteString int1024{0x04, 0x00};
-    CHECK(rlp::encode(int1024) == rlp::ByteString{0x82, 0x04, 0x00});
-
-    rlp::Item set1{empty};
-    rlp::Item set2{empty, set1};
-    rlp::Item set3{empty, set1, set2};
+    CHECK(rlp::encode("dog") == rlp::ByteString{0x83, 'd', 'o', 'g'});
     CHECK(
-      rlp::encode(set3) ==
+      rlp::encode("cat", "dog") ==
+      rlp::ByteString{0xc8, 0x83, 'c', 'a', 't', 0x83, 'd', 'o', 'g'});
+
+    CHECK(rlp::encode(1024) == rlp::ByteString{0x82, 0x04, 0x00});
+
+    CHECK(rlp::encode(rlp::ByteString{0x0}) == rlp::ByteString{0x0});
+
+    CHECK(
+      rlp::encode(rlp::ByteString{0x0, 0x0}) ==
+      rlp::ByteString{0x82, 0x0, 0x0});
+
+    CHECK(rlp::encode(std::make_tuple(0x0)) == rlp::ByteString{0xc1, 0x80});
+    CHECK(
+      rlp::encode(std::make_tuple(0x0, 0x0)) ==
+      rlp::ByteString{0xc2, 0x80, 0x80});
+    CHECK(
+      rlp::encode(std::make_tuple(std::make_tuple(0x0))) ==
+      rlp::ByteString{0xc2, 0xc1, 0x80});
+
+    const auto set_0 = std::make_tuple();
+    CHECK(rlp::encode(set_0) == rlp::ByteString{0xc0});
+
+    const auto set_1 = std::make_tuple(set_0);
+    CHECK(rlp::encode(set_1) == rlp::ByteString{0xc1, 0xc0});
+
+    const auto set_2 = std::make_tuple(set_0, set_1);
+    const auto set_3 = std::make_tuple(set_0, set_1, set_2);
+    CHECK(
+      rlp::encode(set_3) ==
       rlp::ByteString{0xc7, 0xc0, 0xc1, 0xc0, 0xc3, 0xc0, 0xc1, 0xc0});
 
-    std::string lorem(
-      "Lorem ipsum dolor sit amet, consectetur adipisicing elit");
-    rlp::ByteString loremEncoded = rlp::encode(lorem);
-    CHECK(loremEncoded.size() == lorem.size() + 2);
-    CHECK(loremEncoded[0] == 0xb8);
-    CHECK(loremEncoded[1] == 0x38);
-    for (size_t i = 0; i < lorem.size(); ++i)
-    {
-      CHECK(loremEncoded[i + 2] == lorem[i]);
-    }
+    const auto long_and_nested = std::make_tuple(
+      std::make_tuple("Hello world", "Saluton Mondo"),
+      std::make_tuple(
+        std::make_tuple(
+          std::make_tuple(1),
+          std::make_tuple(2, 3),
+          std::make_tuple(std::make_tuple(4))),
+        66000),
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod "
+      "tempor incididunt ut labore et dolore magna aliqua");
+    const auto expected = rlp::to_byte_string(
+      "\xf8\xa5\xda\x8bHello world\x8dSaluton "
+      "Mondo\xcd\xc8\xc1\x01\xc2\x02\x03\xc2\xc1\x04\x83\x01\x01\xd0\xb8zLorem "
+      "ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod "
+      "tempor incididunt ut labore et dolore magna aliqua");
+    CHECK(rlp::encode(long_and_nested) == expected);
+  }
+
+  SUBCASE("uint256_t encode")
+  {
+    const uint256_t a = 1024;
+    CHECK(rlp::encode(a) == rlp::ByteString{0x82, 0x04, 0x00});
+
+    using namespace boost::multiprecision::literals;
+    const uint256_t b = 0x1234567890abcdefdeadbeefcafef00dbaaaad_cppui;
+    CHECK(rlp::encode(b) == rlp::ByteString{0x93, 0x12, 0x34, 0x56, 0x78,
+                                            0x90, 0xab, 0xcd, 0xef, 0xde,
+                                            0xad, 0xbe, 0xef, 0xca, 0xfe,
+                                            0xf0, 0x0d, 0xba, 0xaa, 0xad});
   }
 
   SUBCASE("decode")
@@ -222,8 +235,9 @@ TEST_CASE("addressGeneration" * doctest::test_suite("rlp"))
 
 TEST_CASE("vmExecution" * doctest::test_suite("vm"))
 {
-  // harness.cpp runs more thorough tests from standard test cases. This is the
-  // simplest possible test of the API, independent of json parsing/test formats
+  // harness.cpp runs more thorough tests from standard test cases. This is
+  // the simplest possible test of the API, independent of json parsing/test
+  // formats
 
   SimpleGlobalState gs;
   NullLogHandler ignore;
