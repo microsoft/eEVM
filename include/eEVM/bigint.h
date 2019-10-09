6d0060247a4b82418bbba66fea9a0e647947dbb0
Eddy Ashton <v-edasht@microsoft.com>
2019-10-09 15:07:31 +0100

    Implement ostream operator<< for intx

--- include/eEVM/bigint.h
+++ include/eEVM/bigint.h
@@ -7,4 +7,43 @@
 using uint256_t = intx::uint256;
 using uint512_t = intx::uint512;
 
-// TODO: Implement stream << operators?
+namespace intx
+{
+  template <unsigned N>
+  std::ostream& operator<<(std::ostream& o, const uint<N>& n)
+  {
+    const auto fmt_flags = o.flags();
+    const auto basefield = fmt_flags & std::ostream::basefield;
+    const auto showbase = fmt_flags & std::ostream::showbase;
+
+    switch (basefield)
+    {
+      case (std::ostream::hex):
+      {
+        if (showbase)
+        {
+          o << "0x";
+        }
+        o << to_string(n, 16);
+        break;
+      }
+
+      case (std::ostream::oct):
+      {
+        if (showbase)
+        {
+          o << "0";
+        }
+        o << to_string(n, 8);
+        break;
+      }
+
+      default:
+      {
+        o << to_string(n, 10);
+        break;
+      }
+    }
+    return o;
+  }
+}
--- tests/intx/main.cpp
+++ tests/intx/main.cpp
@@ -2,10 +2,45 @@
 #include <iomanip>
 #include <iostream>
 
-inline auto print_fixed(const intx::uint256& v)
+namespace intx
 {
-  std::cout << "0x" << std::hex << std::setw(40) << std::setfill('0')
-            << intx::hex(v) << std::endl;
+  template <unsigned N>
+  std::ostream& operator<<(std::ostream& o, const uint<N>& n)
+  {
+    const auto fmt_flags = o.flags();
+    const auto basefield = fmt_flags & std::ostream::basefield;
+    const auto showbase = fmt_flags & std::ostream::showbase;
+
+    switch (basefield)
+    {
+      case (std::ostream::hex):
+      {
+        if (showbase)
+        {
+          o << "0x";
+        }
+        o << to_string(n, 16);
+        break;
+      }
+
+      case (std::ostream::oct):
+      {
+        if (showbase)
+        {
+          o << "0";
+        }
+        o << to_string(n, 8);
+        break;
+      }
+
+      default:
+      {
+        o << to_string(n, 10);
+        break;
+      }
+    }
+    return o;
+  }
 }
 
 int main(int argc, char**)
@@ -13,13 +48,22 @@ int main(int argc, char**)
   constexpr auto a = intx::from_string<intx::uint256>("0x542");
   constexpr auto b = intx::uint256{5, 0xa};
   constexpr auto c = intx::uint512{intx::uint256{0xa, 0xb}, 0x4020};
-  std::cout << intx::hex(a) << std::endl;
-  print_fixed(a);
-  std::cout << intx::to_string(a) << std::endl;
-  std::cout << intx::hex(b) << std::endl;
-  std::cout << intx::to_string(b) << std::endl;
-  std::cout << intx::hex(c) << std::endl;
-  std::cout << intx::to_string(c) << std::endl;
+
+  std::cout << std::dec << a << std::endl;
+  std::cout << std::dec << b << std::endl;
+  std::cout << std::dec << c << std::endl;
+
+  std::cout << std::hex << a << std::endl;
+  std::cout << std::hex << b << std::endl;
+  std::cout << std::hex << c << std::endl;
+
+  std::cout << std::oct << a << std::endl;
+  std::cout << std::oct << b << std::endl;
+  std::cout << std::oct << c << std::endl;
+
+  std::cout << std::showbase << std::dec << a << std::endl;
+  std::cout << std::showbase << std::hex << a << std::endl;
+  std::cout << std::showbase << std::oct << a << std::endl;
 
   return 0;
 }
