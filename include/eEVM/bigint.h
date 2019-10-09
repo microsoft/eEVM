f9697719e5be1bef7b221760ff2732b9f371dfb7
Eddy Ashton <v-edasht@microsoft.com>
2019-10-09 18:17:04 +0100

    Explicit [to/from]_json

--- include/eEVM/bigint.h
+++ include/eEVM/bigint.h
@@ -5,12 +5,16 @@
 #include <intx/intx.hpp>
 #include <iomanip>
 #include <iostream>
+#include <nlohmann/json.hpp>
+#include <sstream>
 
 using uint256_t = intx::uint256;
 using uint512_t = intx::uint512;
 
 namespace intx
 {
+  // ostream operator allows easy printing. This should be contributed directly
+  // to intx
   template <unsigned N>
   std::ostream& operator<<(std::ostream& o, const uint<N>& n)
   {
@@ -47,5 +51,27 @@ namespace intx
       }
     }
     return o;
+  }
+
+  // to/from json converters
+  template <unsigned N>
+  void to_json(nlohmann::json& j, const uint<N>& n)
+  {
+    std::stringstream ss;
+    ss << "0x" << to_string(n, 16);
+    j = ss.str();
+  }
+
+  template <unsigned N>
+  void from_json(const nlohmann::json& j, uint<N>& n)
+  {
+    if (!j.is_string())
+    {
+      throw std::runtime_error(
+        "intx numbers can only be parsed from hex-string");
+    }
+
+    const auto s = j.get<std::string>();
+    n = from_string<uint<N>>(s);
   }
 }
